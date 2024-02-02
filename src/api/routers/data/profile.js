82351dabcdbc1bc4b3423677b9c587222fb2cf36
Eorlund Graymane <rkmenon235@gmail.com>
2024-02-02 09:43:20 +0530

    Argument fix for ProfileRouter

--- src/api/routers/data/profile.js
+++ src/api/routers/data/profile.js
@@ -2,11 +2,11 @@ import express from 'express';
 const router = express.Router();
 import profileController from '../../controllers/profileController.js';
 
-router.post('/image', (req,res) => profileController.post.addProfileImage(req, res));
-router.post('/', (req,res) => profileController.post.createProfile(req, res));
+router.post('/image', (req, res) => profileController.post.addProfileImage(req, res));
+router.post('/', (req, res) => profileController.post.createProfile(req, res));
 
-router.get('/image', (req,res) => profileController.get.imageByProfileID(req, res));
-router.get('/', (req,res) => profileController.get.profileByID);
+router.get('/image', (req, res) => profileController.get.imageByProfileID(req, res));
+router.get('/', (req, res) => profileController.get.profileByID(req, res));
 
 
 export default router;
